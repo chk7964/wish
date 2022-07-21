5edc7f872413daaea9e821e355eac2c395765e43
chk7964 <chk7964@yahoo.com>
2022-07-21 08:17:15 +0530

    vs

--- Independence-Day-india/js/main2.js
+++ Independence-Day-india/js/main2.js
@@ -1,13 +1,15 @@
 let shareUrl = 'link.wishes4u.xyz/Independence-Day-India';    // get full url
 let params = new URLSearchParams(window.location.search); // object to get query or strings in url
+let name1=localStorage.getItem("name");
+          document.title = name1 + ' Wish You Happy Indepensday' ;
+
 
     var name =localStorage.getItem("name");
     console.log(name);
 document.getElementById("obj1").innerHTML = name + " wishes";
-document.getElementById("share").href = "whatsapp://send?text= " + name + " wish u *HAPPY INDEPENDANCE DAY* " + shareUrl + "?n=" + name ; 
+document.getElementById("share").href = "whatsapp://send?text= " + name + " wish u *HAPPY INDEPENDANCE DAY* " + shareUrl + "?n=" + name1 ; 
 
-var name1=localStorage.getItem("name");
-          document.title = name1 + ' Wish You Happy Indepensday' ;
+
 
 // Click Body Event
 const body=document.querySelector('body')
