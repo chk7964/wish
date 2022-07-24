40ff9bbe15c68e9329fefffd70e2afe20243d0b9
chk7964 <chk7964@yahoo.com>
2022-07-24 10:33:54 +0530

    vs

--- Independence-Day-India/js/main2.js
+++ Independence-Day-India/js/main2.js
@@ -5,6 +5,7 @@ let name1=localStorage.getItem("name");
 
 
 document.getElementById("obj1").innerHTML = name1;
+document.getElementById("namefooter").innerHTML = "By "+ name1;
 document.getElementById("share").href = "whatsapp://send?text="+"सावन के पवन महीने में "+ "*"+name1+"*" + " की तरफ से आपको wish किया जाता है"+"%0A"+"क्लिक करे और सन्देश देखे👇👇👇👇" +"%0A" + shareUrl + "?by=" + name1.replace("#", "").replace(/ /g, "_") ; 
 
 
